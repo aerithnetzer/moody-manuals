a063a17ac4c39339f534d5bb74af4bcb9b564b4e
Aerith Netzer <aerithnetzer@outlook.com>
2026-08-27 15:07:15 -0500

    Edit extraction

--- scripts/extraction.sh
+++ scripts/extraction.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -A p32234              # Allocation
 #SBATCH -p gengpu
+#SBATCH --gres=gpu:1
 #SBATCH -t 01:00:00             # Walltime/duration of the job
 #SBATCH -N 1                    # Number of Nodes
 #SBATCH --mem=64G               # Memory per node in GB needed for a job. Also see --mem-per-cpu
