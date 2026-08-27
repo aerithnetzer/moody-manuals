c099f5cc6ec7fd01a7e7fba93db36fb4aeceaf35
Aerith Netzer <aerithnetzer@outlook.com>
2026-08-27 15:17:54 -0500

    Make logging path go into subdir

--- src/company_name_extraction.py
+++ src/company_name_extraction.py
@@ -11,7 +11,7 @@ import logging
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "data"
 SCHEMA_PATH = BASE_DIR / "schema.json"
-LOG_PATH = f"{BASE_DIR}/{os.getenv("SLURM_JOB_ID")}"
+LOG_PATH = BASE_DIR / "logs" / f"{os.getenv("SLURM_JOB_ID")}.log"
 logger = logging.getLogger(__name__)
 
 def main():
@@ -27,6 +27,7 @@ def main():
     for batch in batched(os.scandir(DATA_DIR / "raw"), n=8):
         results = extract_images(images=[Image.open(p) for p in batch], schema=str(SCHEMA_PATH), model=model)
         print(results)
+        
 
 
 if __name__ == "__main__":
